89aedff52d70f3a2ba05722d20542e193b04e4a5
Julio-MS <julio.maturana@usm.cl>
2024-09-05 18:58:00 -0400

    Clasificación de Imágenes - Actualización del nombre de recorte del subset - Visualización de imágen con bbox Visualización del recorte con bbox

--- Utils.py
+++ Utils.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from pydicom.pixel_data_handlers.util import apply_voi_lut
 import pandas as pd
+import numpy as np
+import cv2
 
 
 class Utils:
@@ -11,6 +13,7 @@ class Utils:
     findings = pd.read_csv('/Users/julio/Documentos-Local/data/VinDr-Mammo/finding_annotations.csv')
     metadata = pd.read_csv('/Users/julio/Documentos-Local/data/VinDr-Mammo/metadata.csv')
     images_metadata = pd.read_csv('/Users/julio/Documents/PMM/Codigos/Test1/Detection/images_metadata.csv')
+    ss1 = pd.read_csv('/Users/julio/Documents/PMM/Codigos/Test1/Detection/ss1.csv')
 
     def get_path(self, image_id):
         # Filtrar las filas que coincidan con el image_id
@@ -141,3 +144,82 @@ class Utils:
 
         else:
             return "G3"
+
+    def get_array(self, image_id):
+
+        image_path = self.get_path(image_id)
+        dicom = pydicom.dcmread(image_path)
+        arr = dicom.pixel_array
+        return arr
+
+    def win_operation(self, image_name):
+
+        image_id = image_name.split('_')[0]
+        image_path = self.get_path(image_id)
+        dicom = pydicom.dcmread(image_path)
+        img = dicom.pixel_array
+
+        # Extraer Window Center y Window Width
+        WC = dicom.WindowCenter
+        WW = dicom.WindowWidth
+
+        img_windowed = np.zeros_like(img, dtype=np.float32)
+        ymin = 0  # Mínimo valor en la escala de salida
+        ymax = 255  # Máximo valor en la escala de salida
+
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                x = img[i, j]
+                if x <= WC - 0.5 - ((WW - 1) / 2):
+                    img_windowed[i, j] = ymin
+                elif x > WC - 0.5 + ((WW - 1) / 2):
+                    img_windowed[i, j] = ymax
+                else:
+                    img_windowed[i, j] = ((x - (WC - 0.5)) / (WW - 1) + 0.5) * (ymax - ymin) + ymin
+
+        # Convertir la imagen a uint8 para su visualización
+        img_windowed = img_windowed.astype(np.uint8)
+
+        # Agregar BBox
+        img_windowed_bbox = img_windowed.copy()
+
+        # Vértice superior izquierdo
+        x1, y1 = (int(self.ss1.loc[self.ss1['image_id'] == image_id, 'xmin'].values[0]),
+                  int(self.ss1.loc[self.ss1['image_id'] == image_id, 'ymin'].values[0]))
+        # Vértice inferior derecho
+        x2, y2 = (int(self.ss1.loc[self.ss1['image_id'] == image_id, 'xmax'].values[0]),
+                  int(self.ss1.loc[self.ss1['image_id'] == image_id, 'ymax'].values[0]))
+
+        # Dibujar la bounding box en la imagen
+        cv2.rectangle(img_windowed_bbox, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+        # Crop con margen
+        width = x2 - x1
+        height = y2 - y1
+
+        margin_x = int(width * 0.2)
+        margin_y = int(height * 0.2)
+
+        # Ajustar las coordenadas de la bounding box con el margen
+        x1_new = max(x1 - margin_x, 0)  # No permitir valores negativos
+        y1_new = max(y1 - margin_y, 0)
+        x2_new = min(x2 + margin_x, img_windowed.shape[1])  # No permitir valores fuera de los límites de la imagen
+        y2_new = min(y2 + margin_y, img_windowed.shape[0])
+
+        print(f"ROI: ({x1, y1}) ({x2, y2})")
+        print(f"ROI Crop: ({x1_new, y1_new}) ({x2_new, y2_new}")
+
+        # Recortar la región con el margen
+        crop = img_windowed[y1_new:y2_new, x1_new:x2_new]
+
+        # Trasladar las coordenadas de la ROI original a la imagen recortada
+        x1_translated = x1 - x1_new
+        y1_translated = y1 - y1_new
+        x2_translated = x2 - x1_new
+        y2_translated = y2 - y1_new
+        print(f"ROI Crop BBOX: ({x1_translated, y1_translated}) ({x2_translated, y2_translated}")
+
+        crop_bbox = crop.copy()
+        cv2.rectangle(crop_bbox, (x1_translated, y1_translated), (x2_translated, y2_translated), (255, 0, 0), 2)
+
+        return img_windowed, img_windowed_bbox, crop, crop_bbox
